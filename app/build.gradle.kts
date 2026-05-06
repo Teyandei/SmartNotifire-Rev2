9e9a2bd3121be4fc6a5f7a1b3a496e7330df0cc0
T.Yoshizawa <t-yoshi@rc5.so-net.ne.jp>
2026-05-06 20:11:01 +0900

    v2.0.1 リリース

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.teyanday.smartnotifier"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.0.0"
+        versionCode = 16
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
