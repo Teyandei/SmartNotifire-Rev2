1f834d2c7601dfba42570b8f073b42ccd8b4a1e7
T.Yoshizawa <t-yoshi@rc5.so-net.ne.jp>
2026-05-09 21:20:50 +0900

    v2.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.teyanday.smartnotifier"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "2.0.2"
+        versionCode = 18
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
